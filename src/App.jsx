249b517a2b82cc2e8c64a7b5e4e6212c212eaafa
S Adithya <adithyasrikumar03@gmail.com>
2023-12-09 22:40:21 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Note from './components/Note'
+import CreateArea from "./components/CreateArea";
+import './App.css'
+
+const App = () => {
+  const [notes,setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes,newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((item,index) => {
+        return index !== id;
+      });
+    })
+  }
+  return (
+    <div>
+      <Header />
+      
+      <CreateArea 
+        addition = {addNote}
+      />
+
+      {notes.map((item,index) => {
+        return(
+          <Note 
+            key={index}
+            id={index}
+            title={item.title} 
+            content={item.content} 
+            deletion={deleteNote}
+          />);
+      })}
+
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const CreateArea = (props) => {
+    const [note,setNote] = useState({
+        title:"",
+        content:""
+    });
+
+    function handleChange(event) {
+        const {name,value} = event.target;
+        setNote((prevNote) => {
+            return {...prevNote,[name]:value};
+        });
+    }
+
+    function submit(event){
+        event.preventDefault();
+        props.addition(note);
+        setNote({
+            title:"",
+            content:""
+        })
+    }
+
+    return (
+        <div>
+        <form>
+            <input onChange = {handleChange} name="title" placeholder="Title" value={note.title} />
+            <textarea onChange = {handleChange} name="content" placeholder="Take a note..." rows="3" value={note.content} />
+            <button onClick={submit}>Add</button>
+        </form>
+        </div>
+    )
+}
+
+export default CreateArea
